3520258c36101a7bbcb7e18606bc92c9ecb85568
Marcelo Toledo <marcelo@marcelotoledo.com>
2012-04-26 12:06:44 -0300

    Making the email form work well with the database.

--- index.php
+++ index.php
@@ -1,8 +1,38 @@
+<?php
+
+function addEmail($email)
+{
+  $link = mysql_connect('zenpaywwwprod.ciboiymlb7sj.us-east-1.rds.amazonaws.com', 'root', 'rocketinfra');
+  if (!$link) {
+    return false;
+  }
+
+  if (!mysql_select_db('zenpaywww', $link)) {
+    return false;
+  }
+
+  $query = "insert into user (email) values ('$email')";
+  $res = mysql_query($query, $link);
+  if (!$res) {
+    return false;
+  }
+  
+  mysql_close($link);
+}
+
+$email_sent = false;
+if ($_POST["email"]) {
+  addEmail($_POST["email"]);
+  $email_sent = true;
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>ZEN PAY</title>
+	<title>ZENPAY</title>
  	<meta name="description" content="" />
 	<meta name="keywords" content="" />
 	<script type="text/javascript" src="javascript/head.js"></script>
@@ -19,7 +49,7 @@
 
 <div id="root">
 	<header id="top">
-		<h1 id="logo"><a href="./" accesskey="h">Zen Pay</a></h1>
+		<h1 id="logo"><a href="./" accesskey="h">ZENPAY</a></h1>
 		<nav id="skips">
 			<ul>
 				<li><a href="#nav" accesskey="n">Skip to navigation [n]</a></li>
@@ -38,11 +68,12 @@
 			</ul>
 			<div class="box-a">
 				<h2>Cadastre-se e receba nosso kit gratuitamente</h2>
-				<form class="form-a content">
-					<p class="error">Verifique Se Seu E-Mail Foi Digitado Corretamente.</p>
-					<p class="correct">Seu E-Mail Foi Cadastrado Com Sucesso, Em Breve Entraremos Em Contato.</p>
+				<form class="form-a content" action="index.php" method="post">
 					<p class="field">
-						<label for="email">E-mail</label>
+                                                <?php if ($email_sent == true) { ?>
+					            <p class="correct">Seu E-Mail Foi Cadastrado Com Sucesso, Em Breve Entraremos Em Contato.</p>
+                                                <?php } ?>
+						<label for="email">Email</label>
 						<input type="text" id="email" name="email">
 					</p>
 					<p class="action"><button type="submit">Enviar</button></p>
